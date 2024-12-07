619b7d8290d00dd3fdb20f057a497f203e4def3f
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2024-12-07 14:10:36 +0200

    Time: 1 ms (99.66%) | Memory: 41.8 MB (39.65%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] stack = new char[s.length()];
+        int top = -1;
+
+        for(int i =0 ; i<s.length();i++){
+            char c = s.charAt(i);
+            if(c=='(' || c=='[' || c=='{'){
+                stack[++top] = c;
+            }
+            else{
+                if(top == -1){
+                    return false;
+                }
+                char topChar = stack[top--];
+                if((c==')' && topChar != '(')||
+                   (c=='}' && topChar != '{')||
+                   (c==']' && topChar != '[')) {
+                    return false;
+                   }
+                
+            }
+
+        }
+        return top == -1;
+       
+    }
+}
