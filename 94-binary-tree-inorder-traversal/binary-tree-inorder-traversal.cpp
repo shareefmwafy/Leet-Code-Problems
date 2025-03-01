ea0ff05efffa431bf1a08dc0ae58f00e410fbb73
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-03-01 13:01:29 +0200

    Time: 0 ms (100.00%) | Memory: 11 MB (34.39%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.cpp
@@ -6,7 +6,8 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
@@ -14,9 +15,8 @@ public:
     vector<int> inorderTraversal(TreeNode* root) {
         vector<int> result;
         inorder(root, result);
-        return(result);
+        return result;
     }
-
 private:
     void inorder(TreeNode* node, vector<int>& result){
         if(node == nullptr) return;
@@ -27,6 +27,3 @@ private:
     }
 
 };
-
-
-
