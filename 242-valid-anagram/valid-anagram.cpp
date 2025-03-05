5b1fb18425278ef008cd358afafb1ac4e1a067ef
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-03-05 21:58:13 +0200

    Time: 3 ms (54.89%) | Memory: 9.7 MB (74.11%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -5,15 +5,7 @@ public:
         sort(s.begin(), s.end());
         sort(t.begin(), t.end());
 
-        
-        if(s.length() != t.length())
-            return false;
-
-        for(int i = 0; i<s.length(); i++){
-            if(s[i] != t[i])
-                return false;
-        }
-        return true;
+        return s == t;
         
     }
 };
