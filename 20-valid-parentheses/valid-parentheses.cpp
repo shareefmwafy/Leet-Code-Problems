f31291d399bd54db04b41a38fd38de5d35e16ee9
Shareef Mwafy <124365412+shareefmwafy@users.noreply.github.com>
2025-04-06 02:29:51 +0200

    Time: 3 ms (7.43%) | Memory: 8.8 MB (35.78%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,39 @@
+#include <stack>
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> temp;
+        int countA = 0;
+        int countB =0;
+        if(s.length() == 1)
+            return false;
+
+        for(int i =0; i<s.length(); i++){
+            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+                temp.push(s[i]);  
+                countA++;  
+                continue;
+            }
+            else if(temp.empty())
+                return false;
+            else if(!temp.empty()){  
+                if(s[i] == ')' && temp.top() != '('){
+                    return false;
+                } 
+                else if(s[i] == '}' && temp.top() != '{'){
+                    return false;
+                } 
+                else if(s[i] == ']' && temp.top() != '['){
+                    return false;
+                } 
+                countB++;
+                temp.pop();
+            }
+        }
+        if(countA != countB)
+            return false;
+
+
+        return true;
+    }
+};
